a614505f56deb1b35b73e197d697fe3893902332
jcornfoot <jakecornfoot@haslingdenhigh.com>
2024-03-18 19:29:20 +0000

    Updated health so bullets cannot damage player and enemys cannot damage each other

--- Code/Health.cs
+++ Code/Health.cs
@@ -7,19 +7,24 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 100;
     [SerializeField] private float hitPoints;
+    [SerializeField] private GameObject bar;
     [SerializeField] private HealthBar healthBar;
+    [SerializeField] private bool player;
 
     void Start() {
+        healthBar = bar.GetComponent<HealthBar>();
         hitPoints = maxHealth;
         healthBar.UpdateHealthBar(hitPoints, maxHealth);
     }
 
-    public void Hurt(float damage) {
-        hitPoints = Math.Max(hitPoints - damage, 0);
+    public void Hurt(float damage, int type) {
+        if ((!player && type == 0) || (player && type == 1)) {
+            hitPoints = Math.Max(hitPoints - damage, 0);
         healthBar.UpdateHealthBar(hitPoints, maxHealth);
         
         if (hitPoints == 0) {
             Die();
+        }
         }
     }
 
